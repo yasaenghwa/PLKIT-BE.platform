79e61ae5aef824f3a061d9aee1e73cbd39b5c85d
swthewhite <swthewhite@gmail.com>
2024-09-28 12:55:07 +0000

    [BE/#1] feat: add dummy responses

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from typing import Union
+# fastapi 기본 임포트
+from fastapi import FastAPI, HTTPException
+from datetime import datetime
+from typing import Dict, Any
+from pydantic import BaseModel
 
-from fastapi import FastAPI
+# dummy_routes.py에서 라우트 가져오기
+from dummy_routes import router as dummy_router
+from status_routes import router as status_router
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"PLKIT": "DEV"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# dummy 관련 라우트 추가
+app.include_router(dummy_router, prefix="/dummy")
+# status 관련 라우트 추가
+app.include_router(status_router, prefix="/status")
