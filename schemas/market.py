e00e657d852357afb18dccd00f73b4da89f2b39f
swthewhite <swthewhite@gmail.com>
2024-10-31 23:25:30 +0900

    [BE/#23] feat: new schemas updated

--- schemas/market.py
+++ schemas/market.py
@@ -1,8 +1,18 @@
-# app/schemas/market.py
 from pydantic import BaseModel
 from typing import List, Optional
 
 class MarketBase(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
+    crop: Optional[str] = None
+    price: Optional[int] = None
+    location: Optional[str] = None
+    farm_name: Optional[str] = None
+    cultivation_period: Optional[str] = None
+    hashtags: Optional[List[str]] = None
+    image: Optional[str] = None
+
+class MarketCreate(MarketBase):
     title: str
     content: str
     crop: str
@@ -10,15 +20,13 @@ class MarketBase(BaseModel):
     location: str
     farm_name: str
     cultivation_period: str
-    hashtags: Optional[List[str]] = None
-    image: Optional[str] = None
-
-class MarketCreate(MarketBase):
     writer_id: int
+
+class MarketUpdate(MarketBase):
+    pass
 
 class MarketResponse(MarketBase):
     id: int
-    writer: dict
-
+    
     class Config:
         orm_mode = True
