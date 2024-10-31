0e5edc35c4a0a388dfd11a81baf94c3a9ab70cee
swthewhite <swthewhite@gmail.com>
2024-10-31 21:35:37 +0900

    [BE/#21] fix: set model of `user`

--- models/user.py
+++ models/user.py
@@ -1,13 +1,16 @@
 # app/models/user.py
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from database import Base
-
 
 class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)
     name = Column(String, nullable=False)
     avatar = Column(String, nullable=True)
+    password = Column(String, nullable=False)
+
+    # 역방향 관계 설정
+    communities = relationship("Community", back_populates="writer")
