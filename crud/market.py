a675fb1a12a456af15724370291d9260d04e5337
swthewhite <swthewhite@gmail.com>
2024-11-18 19:50:19 +0900

    [BE/#38] add user authentication and authorization for market creation and updates

--- crud/market.py
+++ crud/market.py
@@ -1,3 +1,4 @@
+from http.client import HTTPException, status
 from sqlalchemy.orm import Session
 from models.market import Market
 from schemas.market import MarketCreate, MarketUpdate
@@ -20,10 +21,12 @@ def create_market(db: Session, market_data: MarketCreate):
     db.refresh(market)
     return market
 
-def update_market(db: Session, market_id: int, market_update: MarketUpdate):
+def update_market(db: Session, market_id: int, market_update: MarketUpdate, current_id: int):
     market = db.query(Market).filter(Market.id == market_id).first()
     if not market:
         return None
+    if market.writer_id != current_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="권한이 없습니다.")
     for key, value in market_update.dict(exclude_unset=True).items():
         setattr(market, key, value)
     db.commit()
